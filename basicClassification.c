f58bedc885efeb63c95bf6c89357b6b2e9cb198f
HarelGiladi <93948749+HarelGiladi@users.noreply.github.com>
2021-11-08 23:45:16 +0200

    first

--- basicClassification.c
+++ basicClassification.c
@@ -7,8 +7,7 @@ int isPrime(int x)
 {
    if(x==2){return true;}
    if(x%2==0){return false;}
-   double temp = sqrt((double)x);
-    for(int i =3 ;i<temp;i+=2)
+    for(int i =2 ;i<x;i++)
     {
         if(x%i==0){return false;}
     }
